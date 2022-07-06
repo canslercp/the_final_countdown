f48cc3427d89ebc08f96eb7110dd3f206544cfd4
mmtaylor <taylor.marymargaret@gmail.com>
2022-07-06 11:43:40 -0400

    conole logging joke- added onclick to button

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var button = document.querySelector('.container button');
+var jokeText = document.querySelector('.container p');
+
+async function getJoke(){
+    var jokeData = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+    var jokeObject = await jokeData.json(); 
+    console.log(jokeObject);
+}
+
+// function getJoke(){
+//     var jokeData = fetch('https://icanhazdadjoke.com/', {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     });
+//     var jokeObject = jokeData.json(); 
+//     console.log(jokeObject.joke);
+// }
+
