7e326f872c49db21fe2a7770ec4e9c5170bb77b9
canslercp <canslercp@gmail.com>
2022-07-06 14:44:55 -0400

    debugged cdc api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,31 @@
 var requestUrl = "https://data.cdc.gov/resource/n8mc-b4w4.json?$where=case_month between'2022-05' and '2022-05'";
 
-var state = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SC','TN','TX','UT','VT','VA','WA','WV','WI','WY']
+var state = [{'name':'AL','count':0},'AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM',{'name':'NY','count':0},'NC','ND','OH','OK','OR','PA','RI','SC','SC','TN','TX','UT','VT','VA','WA','WV','WI','WY']
 console.log(state.length)    
 
 $.ajax({
         url: requestUrl,
         type: "GET",
         data: {
-          "$limit" : 1000,
+          "$limit" : 100,
         }
     }).done(function(data) {
         // console.log('Covid cases by state only during 05/2022 \n----------');
         for (var i = 0; i < data.length; i++) {
-          console.log(data[i].res_state.);
+          console.log(data[i].res_state);
 
-          var count = 0;
+          var dataState = data[i].res_state
+          var count = 0
 
-          for (var i = 0; i < state.length; i++){
-            if (data[i].res_state === state[i]){
-              count++;
-
+          for (var x = 0; x < state.length; x++){
+            
+            if (dataState == state[x].name){
+              state[x].count++;
+              console.log(state[x]);
           }
           }
         }
-        console.log(count);
+      
     });
 
     // pseudo code:
